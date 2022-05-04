af1b3cef4a5e25bb529d54c9b4e22a5c28e91a82
dariancmagee <dariancmagee@gmail.com>
2022-05-03 21:47:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import NewMessagForm from './NewMessageForm';
+import MessageList from './MessageList';
 
-function App() {
+export default function App() {
+  const [messages, setMessages] = useState([]);
+  function handleSend(newMessage) {
+    setMessages([newMessage, ...messages]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NewMessagForm onSend={handleSend} />
+      <MessageList data={messages} />
+    </>
   );
 }
-
-export default App;
